d55c6ab005599500b74599811f86da1bf3e0e194
cclmanasa <107193344+cclmanasa@users.noreply.github.com>
2023-10-28 10:32:41 +0530

    Add files via upload

---
+++ My JAVA Foloder/dontno.java
@@ -0,0 +1,9 @@
+public class dontno {
+     public static void main(String[] args) {
+        System.out.println("hello Manasa");
+        int n=5;
+        n=-4;
+         System.out.println(n);
+    }
+
+}
---
+++ My JAVA Foloder/main.java
@@ -0,0 +1,14 @@
+/**
+ * main
+ */
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("hello Manasa");
+        int n=5;
+        n=+3;
+        n=-4;
+         System.out.println(n);
+    }
+
+}
